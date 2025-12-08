78d104823fd31577a29de8a61f54b46396269867
ARTIBHARGAVA21 <bhargavarti7@gmail.com>
2025-12-08 11:04:20 +0530

    Pushing the flow of code

--- start.sh
+++ start.sh
@@ -5,11 +5,10 @@ echo "from django.contrib.auth import get_user_model;
 User=get_user_model();
 import os;
 username=os.getenv('DJANGO_SUPERUSER_USERNAME');
-email=os.getenv('DJANGO_SUPERUSER_EMAIL');
 password=os.getenv('DJANGO_SUPERUSER_PASSWORD');
 print('Checking for existing superuser...');
 if username and password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username=username, email=email, password=password);
+    User.objects.create_superuser(username=username, password=password);
     print('Superuser created.');
 else:
     print('Superuser already exists or env vars missing.');
